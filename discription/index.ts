0c92e842a05892701e4e5a231fc3aabb80001cf9
Tanveer Ali <ali.tanveer1012@gmail.com>
2022-07-27 07:52:52 -0700

    add discription search

--- discription/index.ts
+++ discription/index.ts
@@ -5,6 +5,7 @@ import { Discription } from "../storage/models";
 import { initSequelize } from "../storage/tables";
 import { func500Error, funcSuccess, funcValidationError } from "../src/utils";
 import validateJWTUserSign from "../src/utils/validates";
+import { Op, Sequelize } from "sequelize";
 
 const httpTrigger: AzureFunction = async function (
   context: Context,
@@ -22,14 +23,53 @@ const httpTrigger: AzureFunction = async function (
 
 const get = async (context: Context, req: HttpRequest) => {
   const result = await Discription.findAll({ order: [["createdAt", "DESC"]] });
+
   return funcSuccess(context, {
     discription: result.map((data) => data.toJSON()),
   });
 };
 
 const create = async (context: Context, req: HttpRequest) => {
-  const userPayload = await validateJWTUserSign(context, req) // check if wallet is signed in
-  if (!userPayload || !userPayload?.username) return userPayload
+  if (context?.bindingData?.action === "search") {
+    console.log("abc", context?.bindingData?.action, req.body.input);
+    const result = await Discription.findAll({
+      where: {
+        [Op.or]: [
+          {
+            pre: { [Op.like]: "%" + req.body.input + "%" },
+          },
+          {
+            verb: { [Op.like]: `%${req.body.input}%` },
+          },
+          {
+            first_line: { [Op.like]: `%${req.body.input}%` },
+          },
+          {
+            second_line: { [Op.like]: `%${req.body.input}%` },
+          },
+          Sequelize.where(
+            Sequelize.fn(
+              "concat",
+              Sequelize.col("pre"),
+              Sequelize.col("verb"),
+              Sequelize.col("first_line"),
+              " ",
+              Sequelize.col("second_line")
+            ),
+            {
+              [Op.like]: `%${req.body.input}%`,
+            }
+          ),
+        ],
+      },
+      order: [["createdAt", "DESC"]],
+    });
+    return funcSuccess(context, {
+      discription: result.map((data) => data.toJSON()),
+    });
+  }
+  const userPayload = await validateJWTUserSign(context, req); // check if wallet is signed in
+  if (!userPayload || !userPayload?.username) return userPayload;
 
   const { subject, verb, firstLine, secondLine } = req.body;
 
@@ -53,5 +93,3 @@ const create = async (context: Context, req: HttpRequest) => {
 };
 
 export default httpTrigger;
-
-
